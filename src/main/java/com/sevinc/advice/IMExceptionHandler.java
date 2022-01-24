08773526944d0978e71702de20b0026dbb64f156
yusufsevinc <1yusufsevinc@gmail.com>
2022-01-25 01:38:11 +0300

    exception

--- src/main/java/com/sevinc/advice/IMExceptionHandler.java
+++ src/main/java/com/sevinc/advice/IMExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.sevinc.advice;
 
-public class IMExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class IMExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<?> handleExceptions(Exception ex , WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date() , ex.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
+    }
 }
